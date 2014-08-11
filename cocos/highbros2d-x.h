0d2bdf7b4441e1c509a45560b1149a4a821f1619
이재은 <rap9@highbrow-inc.com>
2014-08-11 10:29:37 +0900

    커밋

---
+++ cocos/highbros2d-x.h
@@ -0,0 +1,18 @@
+//
+//  highbros2d-x.h
+//  cocos2d_libs
+//
+//  Created by 이재은 on 2014. 8. 11..
+//
+//
+
+#ifndef cocos2d_libs_highbros2d_x_h
+#define cocos2d_libs_highbros2d_x_h
+
+/*  macro  */
+#include "highbrow/ui/HBMacros.h"
+
+/*  menu  */
+#include "highbrow/ui/menu/HBMenuItemImage.h"
+
+#endif
